a2cfc7c0b227484a72485fdf52bd5ff72791c05b
agigibairene <i.agigiba@alustudent.com>
2023-02-07 09:55:40 -0800

    I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game

--- python-if_else_loops_functions/2-print_alphabet.py
+++ python-if_else_loops_functions/2-print_alphabet.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-for love in range(ord('a'),ord('z') + 1):
-    print("{}".format(chr(love)), end=" ")
+for x in range(ord('a'), ord('z') + 1):
+    print("{}".format(chr(x)), end=" ")
