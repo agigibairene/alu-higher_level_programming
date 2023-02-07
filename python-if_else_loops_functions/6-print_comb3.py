6a73aef38f1bed4d446418b44dcad6b7400f83a4
agigibairene <i.agigiba@alustudent.com>
2023-02-07 12:49:20 -0800

    Inventing is a combination of brains and materials. The more brains you use, the less material you need

---
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+for x in range(0, 10):
+    for x >= y:
+        continue
+    elif x == 8 and y ==9:
+        print("{}{}".format(x,y))
+    else:
+        print("{}{}".format(x,y), end=", ")
