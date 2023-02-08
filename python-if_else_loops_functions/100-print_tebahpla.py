f4e3e68f4f8aae3ed0ab995f5548b657a69e3bd8
agigibairene <i.agigiba@alustudent.com>
2023-02-08 03:44:57 -0800

    Smile in the mirror

--- python-if_else_loops_functions/100-print_tebahpla.py
+++ python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-for x in range(-ord('z'), -ord('a') -1):
-    if (x % 2) !=0:
-        x = x -(ord('A') - ord('a'))
+for x in range(- ord('z'),(- ord('a') -1)):
+    if (x % 2) != 0:
+        x = x - (ord('A') - ord('a'))
     print("{}".format(chr(-x) ), end="")
