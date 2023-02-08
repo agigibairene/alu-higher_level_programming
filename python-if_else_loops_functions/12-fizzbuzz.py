3ca788910a334d8c5ecfb176c42a36a772d74071
agigibairene <i.agigiba@alustudent.com>
2023-02-08 01:19:19 -0800

    Fizz Buzz

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,4 +1,12 @@
 #!/usr/bin/python3
-fizzbuzz = __import__('12-fizzbuzz').fizzbuzz
-fizzbuzz()
-print("")
+def fizzbuzz():
+    for fizzbuzz in range(1, 101):
+        if fizzbuzz % 3 == 0 and fizzbuzz % 5 == 0:
+            print("Fizzbuzz", end="")
+        if (i % 3) == 0:
+            print("Fizz", end='')
+        if (i % 5) == 0:
+            print("Buzz", end='')
+        if (i % 3) != 0 and (i % 5) != 0:
+            print("{}".format(i), end='')
+        print(" ", end='')
