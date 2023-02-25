f75250aca46de83428bf2ea31190e1d136ec4a36
agigibairene <i.agigiba@alustudent.com>
2023-02-25 11:35:09 -0800

    Secure access to an element in a list

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -3,4 +3,6 @@ def element_at(my_list, idx):
     if idx < 0 or  idx > (len(my_list)):
         return None
     return my_list[idx]
-element_at(my_list, idx)
+my_list =[2,4,6,8,10]
+idx = 2
+print({}.format((idx, element_at(my_list, idx)))
