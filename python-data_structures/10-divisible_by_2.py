5098807503bae8de923b261c5a2e58d89c85b603
agigibairene <i.agigiba@alustudent.com>
2023-03-02 01:26:06 -0800

    Only by 2

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
-    for x in my_list:
-        if x % 2 ==0:
-            return True
+    new_list = my_list.copy()
+    for x in range(len(my_list)):
+        if (my_list[x]) % 2 == 0:
+            new_list[x] = True
         else:
-            return False 
-    return x
+            new_list[x] = False
+    return new_list
