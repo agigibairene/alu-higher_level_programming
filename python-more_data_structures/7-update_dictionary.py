e238cb8c8f7c564b6d3412dac24cfee78e60fe27
agigibairene <i.agigiba@alustudent.com>
2023-03-04 23:41:18 -0800

    Update dictionary

--- python-more_data_structures/7-update_dictionary.py
+++ python-more_data_structures/7-update_dictionary.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
-    values = {key: value}
-    a_dictionary.update(values)
-    return (a_dictionary)
+    for k in a_dictionary.items():
+        if k == key:
+            a_dictionary[k] = value
+            break
+    else:
+        a_dictionary[key] = value
+    return(a_dictionary)
