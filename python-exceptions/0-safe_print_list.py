66e17e6f7331167fcbd7aa9c4ad150ee64dfd2f3
agigibairene <i.agigiba@alustudent.com>
2023-03-06 02:21:28 -0800

    Safe list printing

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     Total = 0
-    for i in range(0,x):
+    for i in range(0, x):
         try:
             print("{}".format(my_list[i]), end="")
             Total += 1
@@ -9,4 +9,3 @@ def safe_print_list(my_list=[], x=0):
             break
     print()
     return Total
-
