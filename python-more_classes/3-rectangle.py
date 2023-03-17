6d3826d47c8e398969faafdc12cd4d0808e42f4d
agigibairene <i.agigiba@alustudent.com>
2023-03-17 02:34:40 -0700

    String representation

--- python-more_classes/3-rectangle.py
+++ python-more_classes/3-rectangle.py
@@ -3,9 +3,9 @@
 
 
 class Rectangle:
-    """defines class Rectangle with
-private instance attributes and 
-public instance methods"""
+    """defines class Rectangle with 
+    private instance attributes (width and height)
+and public instance methods"""
 
     def __init__(self, width=0, height=0):
         self.width = width
@@ -44,14 +44,3 @@ public instance methods"""
         if self.width == 0 or self.height == 0:
             return 0
         return ((2 * self.width) + (2 * self.height))
-
-    def __str__(self):
-        rec_string = ""
-        if self.width == 0 or self.height == 0:
-            return (rec_string)
-        for row in range(self.height):
-            for column in range(self.width):
-                rec_string += "#"
-            rec_string += "\n"
-        rec_string = rec_string[:-1]
-        return (rec_string)
