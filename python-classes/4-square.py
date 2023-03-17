7bb2cfed6176a41d161180bc8a5404fdbdc9a9be
agigibairene <i.agigiba@alustudent.com>
2023-03-16 19:19:22 -0700

    Access and update private attribute

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -20,7 +20,6 @@ and public instance method"""
         elif value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
-
     def area(self):
         """calculates and returns current square area"""
         return(self.__size * self.__size)
