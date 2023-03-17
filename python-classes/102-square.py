e51a85017910eec4f22a433fa7cfc459b22cf620
agigibairene <i.agigiba@alustudent.com>
2023-03-17 13:17:38 -0700

    Compare 2 squares

--- python-classes/102-square.py
+++ python-classes/102-square.py
@@ -1,27 +1,93 @@
 #!/usr/bin/python3
-"""creates class Square with
-private instance attribute size and public instance method"""
+"""
+Module 102-square
+Defines class Square with private size and public area
+Can access and update size
+"""
 
 
 class Square:
-    """defines class with  private instance attribute
-and public instance method"""
+    """
+    class Square definition
+    Args:
+        size (int): size of a side in square
+    Functions:
+        __init__(self, size)
+        size(self)
+        size(self, value)
+        area(self)
+    """
 
     def __init__(self, size=0):
-        self.__size = size
+        """
+        Initializes square
+        Attributes:
+            size (int): defaults to 0 if none; don't use __size to call setter
+        """
+        self.size = size
 
     @property
     def size(self):
-        return(self.__size)
+        """"
+        Getter
+        Return: size
+        """
+        return self.__size
 
     @size.setter
     def size(self, value):
+        """
+        Setter
+        Args:
+            value: sets size to value, if int and >= 0
+        """
         if type(value) is not int:
             raise TypeError("size must be an integer")
         elif value < 0:
             raise ValueError("size must be >= 0")
-        self.__size = value
+        else:
+            self.__size = value
 
     def area(self):
-        """calculates and returns current square area"""
-        return(self.__size * self.__size)
+        """
+        Calculates area of square
+        Returns:
+            area
+        """
+        return (self.__size)**2
+
+    def __eq__(self, other):
+        """
+        Compares and returns if equal
+        """
+        return self.size == other.size
+
+    def __ne__(self, other):
+        """
+        Compares and returns if not equal
+        """
+        return self.size != other.size
+
+    def __lt__(self, other):
+        """
+        Compares and returns if lesser than
+        """
+        return self.size < other.size
+
+    def __le__(self, other):
+        """
+        Compares and returns if lesser than or equal to
+        """
+        return self.size <= other.size
+
+    def __gt__(self, other):
+        """
+        Compares and returns if greater than
+        """
+        return self.size > other.size
+
+    def __ge__(self, other):
+        """
+        Compares and returns if greater than or equal to
+        """
+        return self.size >= other.size
