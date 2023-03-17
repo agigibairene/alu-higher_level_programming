a1f72ba7ca27eb4e8f30a3897972128af28b8686
agigibairene <i.agigiba@alustudent.com>
2023-03-17 14:17:07 -0700

    Singly linked list

--- python-classes/100-singly_linked_list.py
+++ python-classes/100-singly_linked_list.py
@@ -1,20 +1,43 @@
 #!/usr/bin/python3
-"""creates class Node to define linked list node & class SinglyLinkedList"""
+"""
+Module 100-singly_linked_list
+Defines class Node (with private data and next_node)
+"""
 
 
 class Node:
-    """defines class for singly linked list node"""
+    """
+    class Node definition
+    Args:
+        data (int): private
+        next_node : private; can be None or Node object
+    """
 
     def __init__(self, data, next_node=None):
+        """
+        Initializes node
+        Attributes:
+            data (int): private
+            next_node : private; can be None or Node object
+        """
         self.data = data
         self.next_node = next_node
 
     @property
     def data(self):
-        return(self.__data)
+        """"
+        Getter
+        Return: data
+        """
+        return self.__data
 
     @data.setter
     def data(self, value):
+        """
+        Setter
+        Args:
+            value: sets data to value if int
+        """
         if type(value) is not int:
             raise TypeError("data must be an integer")
         else:
@@ -22,51 +45,75 @@ class Node:
 
     @property
     def next_node(self):
-        return(self.__next_node)
+        """"
+        Getter
+        Return: next_node
+        """
+        return self.__next_node
 
     @next_node.setter
     def next_node(self, value):
-        if value is not None and not isinstance(value, Node):
+        """
+        Setter
+        Args:
+            value: sets next_node if value is next_node or None
+        """
+        if type(value) is not Node and value is not None:
             raise TypeError("next_node must be a Node object")
         else:
             self.__next_node = value
 
 
 class SinglyLinkedList:
+    """
+    class SinglyLinkedList definition
+    Args:
+        head: private
+    Functions:
+        __init__(self)
+        sorted_insert(self, value)
+    """
 
     def __init__(self):
+        """
+        Initializes singly linked list
+        Attributes:
+            head: private
+        """
         self.__head = None
 
-    def sorted_insert(self, value):
-        new_node = Node(value)
-        if self.__head is None:
-            self.__head = new_node
-        else:
-            tmp = self.__head
-            if tmp.data > new_node.data:
-                new_node.next_node = tmp
-                self.__head = new_node
-                return
-            while tmp.next_node is not None:
-                tmp2 = tmp.next_node
-                if tmp2.data < new_node.data:
-                    tmp = tmp2
-                else:
-                    new_node.next_node = tmp.next_node
-                    tmp.next_node = new_node
-                    return
-            tmp.next_node = new_node
-
-    def stringrep(self):
-        strrep = ""
+    def __str__(self):
+        """
+        String representation of singly linked list needed to print
+        """
+        string = ""
         tmp = self.__head
         while tmp is not None:
-            datavalue = tmp.data
-            strrep = strrep + str(datavalue)
+            string += str(tmp.data)
             tmp = tmp.next_node
-            if tmp:
-                strrep = strrep + "\n"
-        return strrep
+            if tmp is not None:
+                string += "\n"
+        return string
 
-    def __repr__(self):
-        return(self.stringrep())
+    def sorted_insert(self, value):
+        """
+        Inserts new nodes into singly linked list in sorted order
+        Args:
+        value: int data for node
+        """
+        new = Node(value)
+        if self.__head is None:
+            self.__head = new
+            return
+
+        tmp = self.__head
+        if new.data < tmp.data:
+            new.next_node = self.__head
+            self.__head = new
+            return
+
+        while (tmp.next_node is not None) and (new.data > tmp.next_node.data):
+            tmp = tmp.next_node
+        new.next_node = tmp.next_node
+        tmp.next_node = new
+        return
