e66eba5440658fedb4d093e74cdc7914d13e0e20
agigibairene <i.agigiba@alustudent.com>
2023-03-16 18:46:54 -0700

    Square with size

---
+++ python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" create a class square with a private instance attribute"""
+
+
+class Square:
+    """creates class square"""
+    def __init__(self, size):
+        """create a private instance attribute size"""
+        self.__size = size
