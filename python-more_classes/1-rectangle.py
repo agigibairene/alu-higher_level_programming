eeeec9c9262da1354edfa4547f694ee89f9a2f2e
agigibairene <i.agigiba@alustudent.com>
2023-03-16 23:04:09 -0700

    Real definition of a rectangle

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -3,8 +3,7 @@
 
 
 class Rectangle:
-    """defines class Rectangle with private instance attributes and public instance methods"""
-
+    """defines class Rectangle with private instance attributes and public intance methods"""
     def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
