405f7f39f8294d0a86db145d64c4362bd872093d
agigibairene <i.agigiba@alustudent.com>
2023-03-23 04:39:04 -0700

    To JSON string

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""defines a function that return JSON representation of an object"""
+
+
+def to_json_string(my_obj):
+    """returns JSON representation of an object"""
+    import json
+    return json.dumps(my_obj, ensure_ascii=False)
