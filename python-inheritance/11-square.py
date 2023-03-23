db3a1f319da682c011e0fe961b57431573664388
agigibairene <i.agigiba@alustudent.com>
2023-03-23 14:03:31 -0700

    Square #2

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""defines class Square that inherits from Rectangle"""
+
+
+Rectangle = __import__('8-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """class for square that inherits from Rectangle
+    with method for area and string representation"""
+    def __init__(self, size):
+        self.integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+        return (self.__size * self.__size)
+
+    def __str__(self):
+        str_rep = "[Square] " + str(self.__size) + "/" + str(self.__size)
+        return str_rep
+
