bd9f23e9425eb75dae3779c57ba5a4dc06b2d5fb
agigibairene <i.agigiba@alustudent.com>
2023-03-23 14:12:39 -0700

    Can I?

--- python-inheritance/101-add_attribute.py
+++ python-inheritance/101-add_attribute.py
@@ -7,4 +7,4 @@ def add_attribute(obj, name, value):
     if '__dict__' not in dir(obj):
         raise TypeError("can't add new attribute")
     else:
-        setattr(obj, name, value)`
+        setattr(obj, name, value)
