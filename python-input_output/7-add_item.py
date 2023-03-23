7b201ed70ab6112b8a2860a22130f994f8c516f9
agigibairene <i.agigiba@alustudent.com>
2023-03-23 05:23:43 -0700

    Load, add, save

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -8,9 +8,4 @@ load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 filename = "add_item.json"
 
-try:
-    existing_content = load_from_json_file(filename)
-except FileNotFoundError:
-    existing_content = []
-
 save_to_json_file(existing_content + argv[1:], filename)
