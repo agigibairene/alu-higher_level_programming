ec4e7c9d12d22a005d4a852ebd3823d00f21b3da
agigibairene <i.agigiba@alustudent.com>
2023-03-24 14:08:18 -0700

    Log parsing

--- python-input_output/101-stats.py
+++ python-input_output/101-stats.py
@@ -1,15 +1,20 @@
 #!/usr/bin/python3
-"""a script that reads stdin line by line and computes metrics"""
+"""
+Write a script that reads stdin line by line and computes metrics
+"""
 
 
 def print_stats(size, status_codes):
-    """function print_stats"""
+    """Print accumulated metrics.
+    Args:
+        size (int): The accumulated read file size.
+        status_codes (dict): The accumulated count of status codes.
+    """
     print("File size: {}".format(size))
     for key in sorted(status_codes):
         print("{}: {}".format(key, status_codes[key]))
 
-
-if __name__ == "__main__":
+if _name_ == "_main_":
     import sys
 
     size = 0
