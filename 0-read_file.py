2f090a343a3b2808291a468d9af68f682e3c98d9
agigibairene <i.agigiba@alustudent.com>
2023-03-29 03:03:26 -0700

    My privileges!

--- 0-read_file.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-"""A function that read text file and prints to stdout"""
-
-
-def read_file(filename=""):
-    """reads text file and prints to stdout"""
-    with open(filename) as f:
-        print(f.read(), end="")
