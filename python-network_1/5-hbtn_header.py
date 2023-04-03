eb1a8863bdcbcebcc05c3a6e9ca886bc99aab9e7
agigibairene <i.agigiba@alustudent.com>
2023-04-03 14:04:38 +0000

    Response header value #1

--- python-network_1/5-hbtn_header.py
+++ python-network_1/5-hbtn_header.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-"""A Python script to send request to given URL and 
-display value of X-Request-Id"""
-
-from sys import argv
-import requests
+"""A Python script to send request to given URL 
+and display value of X-Request-Id"""
 
 if __name__ == "__main__":
-    r = requests.get(argv[1])
-    print(r.headers.get('X-Request-Id'))
+    import requests
+    from sys import argv
+    if argv[1]:
+        response = requests.get(argv[1])
+        print(response.headers.get('X-Request-Id'))
