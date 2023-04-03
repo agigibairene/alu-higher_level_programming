c64c32d3abb0424ddbfc3324b1118e7251b3fae0
agigibairene <i.agigiba@alustudent.com>
2023-04-03 13:27:47 +0000

    What's my status? #0

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -4,9 +4,9 @@ Python script that fetches https://alu-intranet.hbtn.io/status
 """
 
 if __name__ == '__main__':
-    from urllib.request import urlopen
-    import sys
-
-    with urlopen(sys.argv[1]) as re:
-        header_var = re.headers.get('X-Request-Id')
-        print(header_var)
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+        content = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(content)))
+        print("\t- content: {}".format(content))
+        print("\t- utf8 content: {}".format(content.decode("utf-8")))
