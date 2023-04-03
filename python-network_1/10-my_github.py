0af782c060f165d207acc401ffd8252dd8342035
agigibairene <i.agigiba@alustudent.com>
2023-04-03 15:21:16 +0000

    My GitHub!

--- python-network_1/10-my_github.py
+++ python-network_1/10-my_github.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
-"""A Python script to take GitHub credentials and 
-uses the GitHub API to display id"""
-if __name__ == "__main__":
+"""A script that takes GitHub credentials."""
+
+if __name__ == '__main__':
     import requests
-    from requests.auth import HTTPBasicAuth as Basic_auth
-    from sys import argv
+    from requests.auth import HTTPBasicAuth
+    import sys
+
     url = 'https://api.github.com/user'
-    try:
-        response = requests.get(url, auth=Basic_auth(argv[1], argv[2]))
-        json_string = response.json()
-        print(json_string['id'])
-    except Exception as err:
-        print("None")
+    user = sys.argv[1]
+    xyz = sys.argv[2]
+    authori = HTTPBasicAuth(username=user, password=xyz)
+    response = requests.get(url, auth=authori)
+    result = response.json()
+    print(result.get('id'))
