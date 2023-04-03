dfa99376dec67e6cdffff45ffad811d6ec045145
agigibairene <i.agigiba@alustudent.com>
2023-04-03 14:33:54 +0000

    My GitHub!

--- python-network_1/10-my_github.py
+++ python-network_1/10-my_github.py
@@ -2,7 +2,6 @@
 """a Python script that takes your
 GitHub credentials (username and password) 
 and uses the GitHub API to display your id"""
-
 if __name__ == "__main__":
     import requests
     from requests.auth import HTTPBasicAuth as Basic_auth
