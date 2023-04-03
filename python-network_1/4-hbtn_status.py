fc8d1a0c25e34d95e10fa736ba9e619ad73202a3
agigibairene <i.agigiba@alustudent.com>
2023-04-03 13:43:20 +0000

    What's my status? #1

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -3,7 +3,7 @@
 
 if __name__ == "__main__":
     import requests
-    response = requests.get('https://alu-intranet.hbtn.io/status')
+    response = requests.get('https://intranet.hbtn.io/status')
     content = response.text
     print("Body response:")
     print("\t- type: {}".format(type(content)))
