0d23115a75336757a8e48ca0c8d48ceff1780897
agigibairene <i.agigiba@alustudent.com>
2023-04-03 12:35:04 +0000

    Response header value #0

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -3,8 +3,6 @@
 value of the X-Request-Id variable'''
 from sys import argv
 import urllib.request
-
-
 if __name__ == "__main__":
     req = urllib.request.Request(argv[1])
     with urllib.request.urlopen(req) as response:
