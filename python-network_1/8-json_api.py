2b6cfadb8cd5268c5c84d4bba4bf626788666af0
agigibairene <i.agigiba@alustudent.com>
2023-04-03 14:09:30 +0000

    Search API

---
+++ python-network_1/8-json_api.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""A Python script that takes in a letter and sends 
+a POST request to http://0.0.0.0:5000/search_user"""
+
+if __name__ == "__main__":
+    import requests
+    from sys import argv
+    data = {}
+    if len(argv) >= 2:
+        data['q'] = argv[1]
+    else:
+        data['q'] = ""
+    response = requests.post('http://0.0.0.0:5000/search_user', data=data)
+    try:
+        json_string = response.json()
+        if json_string == {}:
+            print("No result")
+        else:
+            print("[{}] {}".format(json_string['id'], json_string['name']))
+    except Exception as err:
+        print("Not a valid JSON")
