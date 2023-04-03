67befc3c871af9d62bbf09fc6fdc1f11216895c5
agigibairene <i.agigiba@alustudent.com>
2023-04-03 14:13:55 +0000

    Search API

--- python-network_1/8-json_api.py
+++ python-network_1/8-json_api.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-"""A Python script that takes in a letter and sends 
-a POST request to http://0.0.0.0:5000/search_user"""
-
+"""A Python script that takes in a letter and send
+ a POST request to http://0.0.0.0:5000/search_user"""
 if __name__ == "__main__":
     import requests
     from sys import argv
