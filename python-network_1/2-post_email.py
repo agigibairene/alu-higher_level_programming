024792d890eed7740ca5584cb748199e51c6a825
agigibairene <i.agigiba@alustudent.com>
2023-04-03 13:21:57 +0000

    POST an email #0

--- python-network_1/2-post_email.py
+++ python-network_1/2-post_email.py
@@ -1,16 +1,14 @@
 #!/usr/bin/python3
 """sends a POST request to the passed URL with the email as a parameter"""
-from sys import argv
-import urllib.parse
-import urllib.request
 
 
-if __name__ == "__main__":
-    url = argv[1]
-    values = {'email': argv[2]}
+if __name__ == '__main__':
+    from urllib.request import urlopen, Request
+    from urllib.parse import urlencode
+    import sys
+    data = urlencode({"email": sys.argv[2]
+                      }).encode("ascii")
 
-    data = urllib.parse.urlencode(values)
-    data = data.encode('ascii')
-    req = urllib.request.Request(url, data)
-    with urllib.request.urlopen(req) as response:
+    request = Request(sys.argv[1], data)
+    with urlopen(request) as response:
         print(response.read().decode('utf-8'))
