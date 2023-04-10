f73976d2240034315cf982eaa02940ec33e9676e
agigibairene <i.agigiba@alustudent.com>
2023-04-10 12:29:54 -0700

    Print square

---
+++ python-test_driven_development/4-print_square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""a function that prints a square with the character #"""
+
+def print_square(size):
+    if type(size) is not int:
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    for row in range(size):
+        print("#"*size, end="")
+        print()
