ba40dedaad59f61d72064ca622c605cdd4f16859
agigibairene <i.agigiba@alustudent.com>
2023-04-11 02:34:58 -0700

    Lazy matrix multiplication

--- python-test_driven_development/101-lazy_matrix_mul.py
+++ python-test_driven_development/101-lazy_matrix_mul.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """ a function that multiplies 2 matrices by using the module NumPy"""
-import numpy as np
+import numpy 
 
 
 def lazy_matrix_mul(m_a, m_b):
     """returns the multiple of the twom matrix"""
-    return np.matmul(m_a, m_b)
+    return numpy.matmul(m_a, m_b)
