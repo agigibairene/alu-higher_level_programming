80d5da1b3f17e4421812416da6185e4b4348f26b
agigibairene <i.agigiba@alustudent.com>
2023-04-11 01:52:35 -0700

    Text indentation

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ a function that prints a text with 2 new lines after ., ?, :"""
 
+
 def text_indentation(text):
     """
     Prints text with 2 new lines after each ".", "?", and ":"
