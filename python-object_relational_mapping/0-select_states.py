5bc809d9761cb9ee3a94fcb833b5dd977db6df5e
agigibairene <i.agigiba@alustudent.com>
2023-04-11 21:26:48 -0700

    Get all states

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -6,6 +6,7 @@ import MySQLdb
 from sys import argv
 
 if __name__ == "__main__":
+    #meant to connect database to python file
      db = MySQLdb.connect(host="localhost", user=argv[1],
                          passwd=argv[2], db=argv[3])
     cur = db.cursor()
