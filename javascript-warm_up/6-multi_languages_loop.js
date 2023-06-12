783a49850419d47577f7ab802b5f062a209f2a9b
agigibairene <i.agigiba@alustudent.com>
2023-06-12 03:45:02 +0000

    JavaScript - Warm up

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -1,7 +1,5 @@
 #!/usr/bin/node
-const myVar = ['C is fun', 'Python is cool', 'Javascript is amazing'];
-let i = 0;
-while (i < myVar.length) {
-     console.log(myVar[i]);
-     i++;
+const lines = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+for (let i = 0; i < lines.length; i++) {
+  console.log(lines[i]);
 }
