a851f824de941e2649b505dbaa172a3ab77ac86c
agigibairene <i.agigiba@alustudent.com>
2023-06-12 03:34:20 +0000

    JavaScript - Warm up

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -1,4 +1,4 @@
-#!/usr/bin/node
+i/usr/bin/node
 const { argv } = process;
 const numArg = Number(argv[2]);
 if (!numArg) console.log('Missing number of occurrences');
@@ -6,3 +6,4 @@ else {
   for (let i = 0; i < numArg; i++) {
     console.log('C is fun');
   }
+}
