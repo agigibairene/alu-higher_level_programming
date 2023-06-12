5f891c292de4ce5ea36640dbb996ed1d94bf78a3
agigibairene <i.agigiba@alustudent.com>
2023-06-12 03:52:56 +0000

    JavaScript - Warm up

--- javascript-warm_up/8-square.js
+++ javascript-warm_up/8-square.js
@@ -1,13 +1,17 @@
 #!/usr/bin/node
-const size = parseInt(process.argv[2]);
-if (isNaN(size)) {
+#!/usr/bin/node
+const myVar = 'X';
+const myArgs = process.argv.splice(2);
+const num = Number(myArgs[0]);
+if (isNaN(num)) {
   console.log('Missing size');
 } else {
-  for (let i = 0; i < size; i++) {
-     let value = " ";
-     for (let j = 0; j < size; j++) {
-       value += "X";
-      }
-      console.log(value);
-     }
- }
+  let i = 0;
+  let myString = '';
+  for (i = 0; i < num; i++) {
+    myString += myVar;
+  }
+  for (i = 0; i < num; i++) {
+    console.log(myString);
+  }
+}
