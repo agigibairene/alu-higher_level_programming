014fa0af5b181c9615201cac313600eba1785eb6
agigibairene <i.agigiba@alustudent.com>
2023-06-12 04:01:16 +0000

    JavaScript - Warm up

--- javascript-warm_up/5-to_integer.js
+++ javascript-warm_up/5-to_integer.js
@@ -3,7 +3,7 @@ const input = process.argv[2];
 const isNumber = !isNaN(parseInt(input));
 
 if (isNumber) {
-  console.log(`My number: $number}`);
+  console.log(`My number: ${parseInt(input)}`);
 } else {
   console.log('Not a number');
 }
